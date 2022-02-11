72f08920e8a0fa296ee2f379f3e7665f3722ea2f
Paul J. Durack <me@pauldurack.com>
2022-02-10 22:20:42 -0800

    update template CMIP6 -> 3

--- src/writeJson.py
+++ src/writeJson.py
@@ -347,7 +347,7 @@ for jsonName in masterTargets:
     if jsonName == 'mip_era':
         outFile = ''.join(['../', jsonName, '.json'])
     else:
-        outFile = ''.join(['../CMIP6_', jsonName, '.json'])
+        outFile = ''.join(['../CMIP3_', jsonName, '.json'])
     # Get repo version/metadata - from src/writeJson.py
 
     # Extract last recorded commit for src/writeJson.py
