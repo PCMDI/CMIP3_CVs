612b02c8a244f8f88a4cbbfe6a0eedd955bc8a88
Paul J. Durack <me@pauldurack.com>
2025-06-11 15:45:28 -0700

    update queryCMIP3.py with file/path length info

--- src/queryCMIP3.py
+++ src/queryCMIP3.py
@@ -1,17 +1,20 @@
 #!/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
-import pdb
+# import os
+# import pdb
 import json
 
 """
 Created on Fri Mar 29 09:42:21 2024
 
-PJD 29 Mar 2024    - Initial code, get sorted dates from files
+This script interrogates a json file containing CMIP3 data paths
+
+PJD 29 Mar 2024 - Initial code, get sorted dates from files
+PJD 11 Jun 2025 - Added queries of filename and path lengths; updated to 230723_cmip3.json (was 230703)
 """
 
-f = "230703_cmip3.json"
+f = "230723_cmip3.json"
 with open(f, "r") as fH:
     cm3 = json.load(fH)
 
@@ -19,9 +22,11 @@ print(len(cm3.keys()))
 
 ## loop through entries
 dates = []
+lenPath, lenFileName, lenComp = [[0, 0, 0] for _ in range(3)]
 for count, key in enumerate(cm3.keys()):
     print(count)
     if "!" in key:
+        # skip !_cmorCount, !_fileCount, !bad*
         continue
     elif cm3[key] == {}:
         continue  # '/p/css03/esgf_publish/cmip3/ipcc/20c3m/atm/da/rlus/miub_echo_g/run1'
@@ -33,8 +38,23 @@ for count, key in enumerate(cm3.keys()):
         # print(date)
         dates.append(date)
         # print(dates)
+        # ascertain lengths
+        lenPath1 = len(key)
+        if lenPath1 > lenPath[1]:
+            lenPath = [count, lenPath1, key]
+        lenFileName1 = len(fileKey)
+        if lenFileName1 > lenFileName[1]:
+            lenFileName = [count, lenFileName1, fileKey]
+        lenComp1 = len(key) + len(fileKey)
+        if lenComp1 > lenComp[1]:
+            lenComp = [count, lenComp1, key, fileKey]
 
 ## sort dates
 dates.sort()
 print("first:", dates[0])
 print("last: ", dates[-1])
+
+# %% print len* info
+print("lenPath:    ", lenPath)
+print("lenFileName:", lenFileName)
+print("lenComp:    ", lenComp)
